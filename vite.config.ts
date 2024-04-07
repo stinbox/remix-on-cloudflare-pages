c2925fc0003649bc5cf151297143a82cb43f839b
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2024-04-08 02:36:06 +0900

    コード修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import {
   vitePlugin as remix,
-  cloudflareDevProxyVitePlugin,
+  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
 } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [cloudflareDevProxyVitePlugin(), remix(), tsconfigPaths()],
+  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
 });
